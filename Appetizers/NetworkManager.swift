8c072560926962f5edcb4b3ecb07ab73a8c9ae7f
Seify <seify1@gmail.com>
2023-07-04 18:47:32 +0400

    Remote image loading

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -5,12 +5,15 @@
 //  Created by Roman Smirnov on 03.07.2023.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizersURL = baseURL + "appetizers"
+
+    private let cache = NSCache<NSString, UIImage>()
 
     private init() {}
     func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
@@ -45,5 +48,38 @@ final class NetworkManager {
             }
         }
         task.resume()
+    }
+
+    func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        if let cachedImage = cachedImage(from: urlString) {
+            completed(cachedImage)
+            return
+        }
+
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            self.saveCachedImage(from: urlString, image: image)
+            completed(image)
+        }
+        task.resume()
+    }
+
+    func cachedImage(from urlString: String) -> UIImage? {
+        let cacheKey = NSString(string: urlString)
+        return cache.object(forKey: cacheKey)
+    }
+
+    func saveCachedImage(from urlString: String, image: UIImage) {
+        let cacheKey = NSString(string: urlString)
+        cache.setObject(image, forKey: cacheKey)
     }
 }
---
+++ Appetizers/RemoteImage.swift
@@ -0,0 +1,44 @@
+//
+//  RemoteImage.swift
+//  Appetizers
+//
+//  Created by Roman Smirnov on 04.07.2023.
+//
+
+import SwiftUI
+
+final class ImageLoader: ObservableObject {
+    @Published var image: Image? = nil
+
+    func load(from urlString: String) {
+        NetworkManager.shared.downloadImage(from: urlString) { uiImage in
+            guard let uiImage = uiImage else {
+                return
+            }
+
+            DispatchQueue.main.async {
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+    }
+}
+
+struct RemoteImage: View {
+    var image: Image?
+    var body: some View {
+        let currentImage = image ?? Image("food-placeholder")
+        return currentImage.resizable()
+    }
+}
+
+struct AppetizerRemoteImage: View {
+    @StateObject var imageLoader: ImageLoader = ImageLoader()
+    let urlString: String
+
+    var body: some View {
+        RemoteImage(image: imageLoader.image)
+            .onAppear() {
+                imageLoader.load(from: urlString)
+            }
+    }
+}
